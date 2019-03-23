56e10146df60b5dfb71a65b2e62d3f99236c7b81
carney <skcarney3@gmail.com>
2019-03-23 18:51:51 -0400

    init

---
+++ index.ts
@@ -0,0 +1,133 @@
+import { writeFileSync } from 'fs';
+import { filter, map, chain } from 'lodash';
+import { Trie } from './trie';
+
+const WORD_LENGTH = 5;
+const wordsFile = './testData';
+
+const words = require(wordsFile);
+
+type crossword = Array<Array<string>>; // [['a', 'a', 'a'], ['b', 'b', 'b'], ['c', 'c', 'c']]
+
+function cleanWord(word: string) {
+    let newWord = word.toLowerCase();
+    newWord = newWord.replace(' ', '');
+    return newWord;
+}
+
+const wordsSet = new Set();
+
+const wordsOfLength = chain(words)
+    .filter(word => {
+        const cleaned = cleanWord(word);
+        if (wordsSet.has(cleaned)) {
+            return false;
+        }
+        wordsSet.add(cleanWord(word));
+        return true;
+    })
+    .map(word => {
+        return {
+            originalWord: word,
+            cleanedWord: cleanWord(word),
+        };
+    })
+    .value();
+
+let wordsTrie = new Trie();
+for (let i = 0; i < wordsOfLength.length; i++) {
+    wordsTrie.add(wordsOfLength[i].cleanedWord);
+}
+
+function checkCrosswordStart(crosswordInProgress: crossword) {
+    for (let i = 0; i < WORD_LENGTH - 1; i++) {
+        const startOfWord = [];
+        for (let j = 0; j < crosswordInProgress.length; j++) {
+            startOfWord.push(crosswordInProgress[j][i]);
+        }
+        if (!wordsTrie.containsArrayStart(startOfWord)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function getWordsForCrossword(crossword: crossword) {
+    const words = [];
+    for (let i = 0; i < crossword.length; i++) {
+        const word = crossword[i].join('');
+        words.push(word);
+    }
+    for (let i = 0; i < crossword.length; i++) {
+        let word = '';
+        for (let j = 0; j < crossword.length; j++) {
+            word += crossword[j][i];
+        }
+        words.push(word);
+    }
+    return words;
+}
+const validCrosswords = [];
+
+function validCrossword(crossword: crossword) {
+    const wordsInCrossword = getWordsForCrossword(crossword);
+    const uniqueWords = new Set(wordsInCrossword).size;
+    // crosswords that contain the same word multiple times are ignored
+    if (uniqueWords === WORD_LENGTH * 2) {
+        validCrosswords.push(crossword);
+        console.log('Number of crosswords found:', validCrosswords.length);
+        console.log(crossword);
+        const exports = {
+            crosswords: validCrosswords,
+        };
+        writeFileSync(
+            `crosswords${WORD_LENGTH}.json`,
+            JSON.stringify(exports),
+            'utf8'
+        );
+    }
+}
+
+function testColumn(crossword: crossword, columnWords, columnNum: number) {
+    for (let i = 0; i < columnWords[columnNum].length; i++) {
+        const columnWord = columnWords[columnNum][i];
+        const nextCrossword = [...crossword, columnWord];
+        const check = checkCrosswordStart(nextCrossword);
+
+        if (!check) {
+            continue;
+        }
+        if (columnNum >= WORD_LENGTH - 1) {
+            validCrossword(nextCrossword);
+        } else {
+            const nextNum = columnNum + 1;
+            testColumn(nextCrossword, columnWords, nextNum);
+        }
+    }
+}
+
+for (let h = 0; h < wordsOfLength.length; h++) {
+    if (h % 50 === 0) {
+        console.log(`${h}/${wordsOfLength.length} starting words tested`);
+    }
+    const startingWord = wordsOfLength[h].cleanedWord;
+    const crossword = [startingWord.split('')];
+    const columnWords = [];
+    for (let i = 0; i < crossword[0].length; i++) {
+        const topLetter = crossword[0][i];
+        const wordsInThatColumn = map(
+            filter(
+                wordsOfLength,
+                ({ cleanedWord }) =>
+                    cleanedWord.substr(0, 1) === topLetter &&
+                    cleanedWord !== startingWord
+            ),
+            ({ cleanedWord }) => cleanedWord.split('')
+        );
+        columnWords.push(wordsInThatColumn);
+    }
+
+    testColumn([], columnWords, 0);
+    process.exit();
+}
---
+++ trie.ts
@@ -0,0 +1,191 @@
+function NodeChild(data) {
+    this.data = data;
+    this.isWord = false;
+    this.prefixes = 0;
+    this.children = {};
+}
+
+export function Trie() {
+    this.root = new NodeChild('');
+}
+
+Trie.prototype.add = function(word) {
+    if (!this.root) {
+        return null;
+    }
+    this._addNode(this.root, word);
+};
+Trie.prototype._addNode = function(node, word) {
+    if (!node || !word) {
+        return null;
+    }
+    node.prefixes++;
+    var letter = word.charAt(0);
+    var child = node.children[letter];
+    if (!child) {
+        child = new NodeChild(letter);
+        node.children[letter] = child;
+    }
+    var remainder = word.substring(1);
+    if (!remainder) {
+        child.isWord = true;
+    }
+    this._addNode(child, remainder);
+};
+
+Trie.prototype.contains = function(word) {
+    if (!this.root) {
+        return false;
+    }
+    return this._contains(this.root, word);
+};
+Trie.prototype._contains = function(node, word) {
+    if (!node || !word) {
+        return false;
+    }
+    var letter = word.charAt(0);
+    var child = node.children[letter];
+    if (child) {
+        var remainder = word.substring(1);
+        if (!remainder && child.isWord) {
+            return true;
+        } else {
+            return this._contains(child, remainder);
+        }
+    } else {
+        return false;
+    }
+};
+
+function sliceFirst(base) {
+    var newArray = [];
+    for (var i = 1; i < base.length; i++) {
+        newArray[i - 1] = base[i];
+    }
+    return newArray;
+}
+
+Trie.prototype.containsArray = function(word) {
+    return this._containsArray(this.root, word);
+};
+Trie.prototype._containsArray = function(node, word) {
+    var child = node.children[word[0]];
+    if (child) {
+        const newArray = sliceFirst(word);
+        if (newArray.length === 0 && child.isWord) {
+            return true;
+        } else {
+            return this._containsArray(child, newArray);
+        }
+    } else {
+        return false;
+    }
+};
+
+Trie.prototype.containsArrayStart = function(word) {
+    return this._containsArrayStart(this.root, word);
+};
+Trie.prototype._containsArrayStart = function(node, word) {
+    var child = node.children[word[0]];
+    if (child) {
+        const newArray = sliceFirst(word);
+        if (newArray.length === 0) {
+            return true;
+        } else {
+            return this._containsArrayStart(child, newArray);
+        }
+    } else {
+        return false;
+    }
+};
+
+Trie.prototype.countWords = function() {
+    if (!this.root) {
+        return console.log('No root node found');
+    }
+    var queue = [this.root];
+    var counter = 0;
+    while (queue.length) {
+        var node = queue.shift();
+        if (node.isWord) {
+            counter++;
+        }
+        for (var child in node.children) {
+            if (node.children.hasOwnProperty(child)) {
+                queue.push(node.children[child]);
+            }
+        }
+    }
+    return counter;
+};
+Trie.prototype.getWords = function() {
+    var words = [];
+    var word = '';
+    this._getWords(this.root, words, words, word);
+    return words;
+};
+Trie.prototype._getWords = function(node, words, word) {
+    for (var child in node.children) {
+        if (node.children.hasOwnProperty(child)) {
+            word += child;
+            if (node.children[child].isWord) {
+                words.push(word);
+            }
+            this._getWords(node.children[child], words, word);
+            word = word.substring(0, word.length - 1);
+        }
+    }
+};
+
+Trie.prototype.printByLevel = function() {
+    if (!this.root) {
+        return console.log('No root node found');
+    }
+    var newline = new NodeChild('\n');
+    var queue = [this.root, newline];
+    var string = '';
+    while (queue.length) {
+        var node = queue.shift();
+        string += node.data.toString() + (node.data !== '\n' ? ' ' : '');
+        if (node === newline && queue.length) {
+            queue.push(newline);
+        }
+        for (var child in node.children) {
+            if (node.children.hasOwnProperty(child)) {
+                queue.push(node.children[child]);
+            }
+        }
+    }
+    console.log(string.trim());
+};
+
+Trie.prototype.print = function() {
+    if (!this.root) {
+        return console.log('No root node found');
+    }
+    var newline = new NodeChild('|');
+    var queue = [this.root, newline];
+    var string = '';
+    while (queue.length) {
+        var node = queue.shift();
+        string += node.data.toString() + ' ';
+        if (node === newline && queue.length) {
+            queue.push(newline);
+        }
+        for (var child in node.children) {
+            if (node.children.hasOwnProperty(child)) {
+                queue.push(node.children[child]);
+            }
+        }
+    }
+    console.log(string.slice(0, -2).trim());
+};
+
+var trie = new Trie();
+trie.add('one');
+trie.add('otf');
+trie.add('otb');
+// trie.add('two');
+// trie.add('fifth');
+// trie.add('fifty');
+// trie.printByLevel();
